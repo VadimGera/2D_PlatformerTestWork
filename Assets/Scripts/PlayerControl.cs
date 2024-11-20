d9b50cda7d3697407dd3fab6526500f89f38b6ad
Вадим Гермашев <124063584+VadimGera@users.noreply.github.com>
2024-11-20 19:57:50 +0300

    fix

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,17 +6,28 @@ public class PlayerControl : MonoBehaviour
     public float jumpForce = 10f;
     
     private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
     private bool isGrounded;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb2d.velocity = new Vector2(moveInput * moveSpeed, rb2d.velocity.y);
+
+        if (moveInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
 
         if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
